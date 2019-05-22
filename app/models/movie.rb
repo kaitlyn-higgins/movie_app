980c8d7a0c9c9dcd3cc4de372866d2dcb045b817
kaitlyn_higgins <kaitlyn.brooke.higgins@gmail.com>
2019-05-21 22:36:58 -0500

    Associates actors with movies where each movie can have many actors

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,7 @@
 class Movie < ApplicationRecord
 
+  has_many :actors
+
   validates :title, length: { minimum: 2 }
   validates :year, numericality: { only_integer: true }, length: { is: 4 }
   validates :plot, presence: true
