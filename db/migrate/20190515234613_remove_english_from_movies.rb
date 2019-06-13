3e36202034e316d8ab6bd5a6fe029f532915f8de
Kaitlyn Higgins <kaitlynhiggins@Kaitlyns-MacBook-Pro.local>
2019-06-13 00:27:10 -0500

    update for heroku push

--- db/migrate/20190515234613_remove_english_from_movies.rb
+++ db/migrate/20190515234613_remove_english_from_movies.rb
@@ -1,5 +1,5 @@
 class RemoveEnglishFromMovies < ActiveRecord::Migration[5.2]
   def change
-    remove_column :movies, :english?, :string
+    # remove_column :movies, :english?, :string
   end
 end
